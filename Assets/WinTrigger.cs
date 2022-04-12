4a20a4bb17066cbd393a27d832e8d3553ec78f9e
Wi024933 <beattywf@knights.ucf.edu>
2022-04-12 15:15:19 -0400

    yes

---
+++ Assets/WinTrigger.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinTrigger : MonoBehaviour
+{
+    //---------------------------//
+    void OnTriggerEnter2D(Collider2D collider)
+    //---------------------------//
+    {
+
+        if (collider.tag == "Player")
+        {
+            Debug.Log("Ya did it");
+            SceneManager.LoadScene(0);
+        }
+
+    }//END OnTriggerEnter2D
+
+
+}//END WinTrigger
